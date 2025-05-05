5f0d2b7e64df0d33bf1c081ecfe508b2ac72fba6
CarlosMG06 <carlos.abmg06@gmail.com>
2025-05-05 17:48:15 +0200

    Create MilitaryUnit.java

---
+++ M3/MilitaryUnit.java
@@ -0,0 +1,12 @@
+package proyectoFinal;
+
+interface MilitaryUnit {
+	abstract int attack();
+	abstract void takeDamage(int receivedDamage);
+	abstract int getActualArmor();
+	abstract int getMetalCost();
+	abstract int getDeuteriumCost();
+	abstract int getChanceGeneratingWaste();
+	abstract int getChanceAttackAgain();
+	abstract void resetArmor();
+}
