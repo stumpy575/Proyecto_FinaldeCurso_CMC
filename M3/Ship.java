a3c55808dccb0b7d480d0fd0e71ff1e7d794a2ca
CNG97 <sdvsdafgsdfa@gmail.com>
2025-05-06 17:12:03 +0200

    Medio Updt Ship
    hay que hacer primero los metodos de planet, los constructores se tienen que cambiar

--- M3/Ship.java
+++ M3/Ship.java
@@ -131,5 +131,130 @@ public class HeavyHunter extends Ship{
 		
 	}
 	
+}
+
+public class BattleShip extends Ship {
+	
+	public BattleShip(int armor, int baseDamage) {
+		super();
+		armor=Ship.ARMOR_BATTLESHIP+(tecnologiadefensa*Ship.PLUS_ARMOR_BATTLESHIP_BY_TECHNOLOGY/Ship.ARMOR_BATTLESHIP);
+		baseDamage=Ship.BASE_DAMAGE_BATTLESHIP+(tecnologiaataque*Ship.PLUS_ATTACK_BATTLESHIP_BY_TECHNOLOGY/Ship.BASE_DAMAGE_BATTLESHIP);
+	}
+	public BattleShip() {
+		armor=Ship.ARMOR_BATTLESHIP;
+		baseDamage=Ship.BASE_DAMAGE_BATTLESHIP;
+		
+	}
+
+	@Override
+	public int attack() {
+		// TODO Auto-generated method stub
+		return baseDamage;
+	}
+
+	@Override
+	public void takeDamage(int receivedDamage) {
+		armor-=receivedDamage;
+		
+	}
+
+	@Override
+	public int getActualArmor() {
+		// TODO Auto-generated method stub
+		return armor;
+	}
+
+	@Override
+	public int getMetalCost() {
+		// TODO Auto-generated method stub
+		return Ship.METAL_COST_BATTLESHIP;
+	}
+
+	@Override
+	public int getDeuteriumCost() {
+		// TODO Auto-generated method stub
+		return Ship.DEUTERIUM_COST_BATTLESHIP;
+	}
+
+	@Override
+	public int getChanceGeneratingWaste() {
+		// TODO Auto-generated method stub
+		return Ship.CHANCE_GENERATNG_WASTE_BATTLESHIP;
+	}
+
+	@Override
+	public int getChangeAttackAgain() {
+		// TODO Auto-generated method stub
+		return Ship.CHANCE_ATTACK_AGAIN_BATTLESHIP;
+	}
+
+	@Override
+	public void resetArmor() {
+		armor=initialArmor;
+		
+	}
+	
+}
+
+public class ArmoredShip extends Ship {
+	
+	public ArmoredShip(int armor, int baseDamage) {
+		super();
+		armor=Ship.ARMOR_ARMOREDSHIP+(tecnologiadefensa*Ship.PLUS_ARMOR_ARMOREDSHIP_BY_TECHNOLOGY/Ship.ARMOR_ARMOREDSHIP);
+		baseDamage=Ship.BASE_DAMAGE_ARMOREDSHIP+(tecnologiaataque*Ship.PLUS_ATTACK_ARMOREDSHIP_BY_TECHNOLOGY/Ship.BASE_DAMAGE_ARMOREDSHIP);
+	}
+	public ArmoredShip() {
+		armor=Ship.ARMOR_ARMOREDSHIP;
+		baseDamage=Ship.BASE_DAMAGE_ARMOREDSHIP;
+		
+	}
+
+	@Override
+	public int attack() {
+		// TODO Auto-generated method stub
+		return baseDamage;
+	}
+
+	@Override
+	public void takeDamage(int receivedDamage) {
+		armor-=receivedDamage;
+		
+	}
+
+	@Override
+	public int getActualArmor() {
+		// TODO Auto-generated method stub
+		return armor;
+	}
+
+	@Override
+	public int getMetalCost() {
+		// TODO Auto-generated method stub
+		return Ship.METAL_COST_ARMOREDSHIP;
+	}
+
+	@Override
+	public int getDeuteriumCost() {
+		// TODO Auto-generated method stub
+		return Ship.DEUTERIUM_COST_ARMOREDSHIP;
+	}
+
+	@Override
+	public int getChanceGeneratingWaste() {
+		// TODO Auto-generated method stub
+		return Ship.CHANCE_GENERATNG_WASTE_ARMOREDSHIP;
+	}
+
+	@Override
+	public int getChangeAttackAgain() {
+		// TODO Auto-generated method stub
+		return Ship.CHANCE_ATTACK_AGAIN_ARMOREDSHIP;
+	}
+
+	@Override
+	public void resetArmor() {
+		armor=initialArmor;
+		
+	}
 	
 }
