d754f9a42c50235194573d80589410ea6c87f812
CarlosMG06 <carlos.abmg06@gmail.com>
2025-05-07 21:09:14 +0200

    (menor) getter de planetArmy

--- M3/Battle.java
+++ M3/Battle.java
@@ -58,11 +58,13 @@ public class Battle implements Variables{
 		
 		int winningSide = calculateWinner();
 		generateBattleStatistics(winningSide);
+		
+		planetArmy = armies[0];
 		if (winningSide == 0) {
 			return true;
 		} 
 		return false;
-	}
+	} 
 	
 	private void initInitialArmies() {
 		for (int i = 0; i < planetArmy.length; i++) {
@@ -257,5 +259,9 @@ public class Battle implements Variables{
 
 	public String getBattleDevelopmentLog() {
 		return battleDevelopmentLog;
+	}
+
+	public ArrayList<MilitaryUnit>[] getPlanetArmy() {
+		return planetArmy;
 	}
 }
