e267e17064b848812aa25d48fd27446ab88c76f8
CarlosMG06 <carlos.abmg06@gmail.com>
2025-05-19 13:57:53 +0200

    Main probablemente arreglado

--- M3/Main_old.java
+++ M3/Main_old.java
@@ -2,13 +2,18 @@ package proyectoFinal;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main_old {
 	private static boolean consoleMode;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+<<<<<<< HEAD
+		System.out.println("Enable Console Mode? (y/n)");
+		if (sc.next().toLowerCase().equals("y")) {
+=======
 		System.out.println("Enable Graphical Mode? (y/n)");
 		if (sc.next().toLowerCase().equals("n")) {
+>>>>>>> marc
 			consoleMode = true;
 			System.out.println("CONSOLE MODE ENABLED\n");
 			ConsoleUI console_ui = new ConsoleUI();
@@ -17,6 +22,10 @@ public class Main {
 			consoleMode = false;
 			System.out.println("Starting game...");
 			GraphicalUI graphical_ui = new GraphicalUI();
+<<<<<<< HEAD
+			graphical_ui.start();
+=======
+>>>>>>> marc
 		}
 	}
 
